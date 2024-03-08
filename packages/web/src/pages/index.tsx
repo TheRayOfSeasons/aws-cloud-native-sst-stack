36291776e80cc46222625d2da2ac2ccda3b4eda6
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-03-08 17:08:58 +0800

    Add: basic list display for dashboard

--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { type RouteComponentProps } from '@reach/router';
+import { useAuth } from '../store/auth/store';
+import { useNotes } from '../store/notes/store';
 
 interface Props extends RouteComponentProps {}
 
 export const IndexPage: React.FC<Props> = () => {
+  const { logout } = useAuth();
+  const { list, noteList } = useNotes();
+
+  useEffect(() => {
+    list();
+  }, [list]);
+
   return (
-    <></>
+    <>
+      <button type="button" onClick={logout}>Logout</button>
+      <pre>
+        {JSON.stringify(noteList, null, 2)}
+      </pre>
+    </>
   );
 };
