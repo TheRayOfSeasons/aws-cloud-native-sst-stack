785740fddfe8065cf064f413a438f40ee1a51462
Ray Lawrence Henri Sison <raylawrencehenrisison@gmail.com>
2024-03-08 17:04:50 +0800

    Add: fundamental functionalities to base layouts

--- packages/web/src/layouts/AuthLayout.tsx
+++ packages/web/src/layouts/AuthLayout.tsx
@@ -7,8 +7,15 @@ interface Props extends RouteComponentProps {
 
 export const AuthLayout: React.FC<Props> = ({ children }) => {
   return (
-    <>
-      {children}
-    </>
+    <div className="w-screen min-h-screen flex flex-col">
+      <div className="flex-1 flex flex-row justify-center items-center">
+        <div className="min-w-[500px] min-h-[200px] m-8">
+          {children}
+        </div>
+      </div>
+      <div className="text-center border-t-[2px] border-black">
+        <p>{'AWS Cloud Native SST Stack • MIT License • Copyright (c) 2024 Ray Lawrence Henri Sison'}</p>
+      </div>
+    </div>
   );
 };
--- packages/web/src/layouts/DashboardLayout.tsx
+++ packages/web/src/layouts/DashboardLayout.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import { type RouteComponentProps } from '@reach/router';
+import React, { useEffect } from 'react';
+import { type RouteComponentProps, useNavigate } from '@reach/router';
+import { useAuth } from '../store/auth/store';
 
 interface Props extends RouteComponentProps {
   children: React.ReactNode
 }
 
 export const DashboardLayout: React.FC<Props> = ({ children }) => {
+  const navigate = useNavigate();
+  const { token } = useAuth();
+  
+  useEffect(() => {
+    if (!token) {
+      navigate('/auth');
+    }
+  }, [navigate, token]);
+
   return (
     <>
       {children}
